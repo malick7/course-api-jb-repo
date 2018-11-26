dcf40bae0b588ce9560713c2cad0d983867da200
Saleem Malick P M <saleem.pm@tavant.com>
2018-11-26 21:35:15 +0530

    Refactored - removed hard coded values

--- src/main/java/com/malickpm/springbootstarter/topic/TopicService.java
+++ src/main/java/com/malickpm/springbootstarter/topic/TopicService.java
@@ -12,18 +12,8 @@ public class TopicService {
 	
 	@Autowired
 	private TopicRepository topicRepository;
-
-	private List<Topic> topics = new ArrayList<Topic>(
-									//Arrays.asList returns a immutable list
-									Arrays.asList(
-									new Topic("spring", "Spring Fw", "Spring FW desc"),
-									new Topic("java", "Core Java", "Core Java desc"),
-									new Topic("javascript", "Javascript", "Javascript desc")
-									)
-								);	
 	
 	public List<Topic> getAllTopics(){
-		//return topics;
 		List<Topic> topics = new ArrayList<Topic>();
 		
 		topicRepository.findAll() //returns Iterable
@@ -33,33 +23,16 @@ public class TopicService {
 	}
 	
 	public Topic getTopicById(String id){
-		//return topics.stream().filter(t-> t.getId().equals(id)).findFirst().get();
 		
 		return topicRepository.findOne(id);
 	}
 	public void addTopic(Topic topic){
-		//topics.add(topic);
 		
 		topicRepository.save(topic);
 	}
 
 	public void updateTopic(String id, Topic topic) {
-		/*for(int i=0; i< topics.size(); i++)	{
-			Topic tempTopic = topics.get(i);
-			if(tempTopic.getId().equals(id)){
-				//topic.setDescription(topic.getDescription());
-				//topic.setName(topic.getName());				
-				topics.set(i, topic);
-				return;
-			}
-		}*/
-		
-		//topics.stream().filter(t->t.getId().equals(id))
-		//  .map(t->topics.set(topics.indexOf(t), topic))
-		//  .collect(Collectors.toList());﻿
-		
 		topicRepository.save(topic); // same as add! does update or add based on ID at runtime!!!
-
 	}
 
 	public void deleteTopicById(String id) {
